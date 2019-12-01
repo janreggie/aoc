2f55e7c9e9434937dac38986bbddd0d376c1984f
janreggie <janreggie.delacruz@gmail.com>
2019-12-01 20:15:17 +0800

    2016!

---
+++ aoc2016/day01.go
@@ -0,0 +1,137 @@
+package aoc2016
+
+import (
+	"bufio"
+	"fmt"
+	"image"
+	"strconv"
+	"unicode"
+	"unicode/utf8"
+)
+
+type direction int
+
+const (
+	// B0 indicates NS or EW; B1 indicates positive or negative
+	north direction = iota // 0b00
+	east                   // 0b01
+	south                  // 0b10
+	west                   // 0b11
+)
+
+// checkPointInSlice: return true if in slice
+func checkPointInSlice(slice []image.Point, pt image.Point) bool {
+	for _, v := range slice {
+		if v == pt {
+			return true
+		}
+	}
+	return false
+}
+
+func absInt64(a int64) int64 {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func readUntilCommaSpace(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	// Skip leading spaces.
+	start := 0
+	for width := 0; start < len(data); start += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[start:])
+		if !unicode.IsSpace(r) {
+			break
+		}
+	}
+	// Scan until comma or when space, marking end of word.
+	for width, i := 0, start; i < len(data); i += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[i:])
+		if r == ',' || unicode.IsSpace(r) {
+			return i + width, data[start:i], nil
+		}
+	}
+	// If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
+	if atEOF && len(data) > start {
+		return len(data), data[start:], nil
+	}
+	// Request more data.
+	return start, nil, nil
+}
+
+// movePosition: return image.Point which is old offset by amt in direction
+func movePosition(old image.Point, dir direction, amt int64) image.Point {
+	ns := dir%2 == 0            // true for NS; false for EW
+	positive := (dir>>1)%2 == 0 // true for POSITIVE effect; false for NEGATIVE
+	new := image.Pt(0, 0)
+	if ns {
+		if positive {
+			new = old.Add(image.Pt(0, int(amt)))
+		} else {
+			new = old.Add(image.Pt(0, int(-amt)))
+		}
+	} else {
+		if positive {
+			new = old.Add(image.Pt(int(amt), 0))
+		} else {
+			new = old.Add(image.Pt(int(-amt), 0))
+		}
+	}
+	return new
+}
+
+// Day01 solves the first day puzzle
+// "No Time for a Taxicab"
+func Day01(scanner *bufio.Scanner) (answer1, answer2 string, e error) {
+	currentDirection := north
+	current := image.Pt(0, 0) // current position
+	allVisited := make([]image.Point, 0, 32)
+	found := false // have we found the intersection?
+	intersection := image.Pt(0, 0)
+	scanner.Split(readUntilCommaSpace)
+	for scanner.Scan() {
+		raw := scanner.Text() // raw == [R|L](count)
+		// check direction
+		if len(raw) < 2 {
+			e = fmt.Errorf("invalid string: `%v`", raw)
+			return
+		}
+		if raw[0] == 'R' {
+			currentDirection++
+		} else if raw[0] == 'L' {
+			currentDirection--
+		} else {
+			e = fmt.Errorf("invalid string: `%v`", raw)
+			return
+		}
+		currentDirection = currentDirection % 4
+
+		amt, err := strconv.ParseInt(raw[1:], 10, 64)
+		if err != nil {
+			e = err
+			return
+		}
+
+		new := movePosition(current, currentDirection, amt)
+		if !found {
+			trans := current
+			for i := int64(0); i < amt; i++ {
+				trans = movePosition(trans, currentDirection, 1)
+				if !checkPointInSlice(allVisited, trans) {
+					allVisited = append(allVisited, trans)
+				} else {
+					found = true
+					intersection = trans
+				}
+			}
+		}
+		current = new
+	}
+
+	answer1 = strconv.FormatInt(absInt64(int64(current.X))+absInt64(int64(current.Y)), 10)
+	answer2 = strconv.FormatInt(absInt64(int64(intersection.X))+absInt64(int64(intersection.Y)), 10)
+	return
+}
---
+++ aoc2016/day01_test.go
@@ -0,0 +1,18 @@
+package aoc2016_test
+
+import (
+	"testing"
+
+	"github.com/janreggie/AdventOfCode/aoc2016"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDay01(t *testing.T) {
+	assert := assert.New(t)
+	testCases := []testCase{
+		{"R8, R4, R4, R8", "8", "4"},
+	}
+	for _, eachCase := range testCases {
+		testTestCase(eachCase, aoc2016.Day01, assert)
+	}
+}
