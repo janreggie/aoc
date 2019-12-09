2bfaa8ae42f92a117db69f4c819ec09116c3b779
janreggie <janreggie.delacruz@gmail.com>
2019-12-10 00:26:14 +0800

    Day 8! Needs some tidying up..

--- aoc2019/init.go
+++ aoc2019/init.go
@@ -17,7 +17,7 @@ var AllSolutions = []func(*bufio.Scanner) (string, string, error){
 	nothing, // day 5
 	Day06,   // day 6
 	nothing, // day 7
-	nothing, // day 8
+	Day08,   // day 8
 	nothing, // day 9
 	nothing, // day 10
 	nothing, // day 11
