bea90c9acb4b6189b095bbe7d385ada43ea12341
janreggie <janreggie.delacruz@gmail.com>
2019-12-22 14:40:02 +0800

    Cleaned up Y2015D03

--- aoc2015/day03.go
+++ aoc2015/day03.go
@@ -3,59 +3,59 @@ package aoc2015
 import (
 	"bufio"
 	"fmt"
+	"image"
 	"strconv"
 )
 
+// iterateCoordinate iterates the location using an input
+func iterateCoordinate(location *image.Point, input string) {
+	switch input {
+	case "v":
+		location.Y--
+	case "^":
+		location.Y++
+	case "<":
+		location.X--
+	case ">":
+		location.X++
+	}
+}
+
 // Day03 solves the third day puzzle
 // "Perfectly Spherical Houses in a Vacuum"
-func Day03(scanner *bufio.Scanner) (string, string, error) {
-	answer1, answer2 := "", ""
+func Day03(scanner *bufio.Scanner) (answer1, answer2 string, err error) {
 	scanner.Split(bufio.ScanBytes)
-	iterateCoordinate := func(coordX, coordY *int32, input string) {
-		switch input {
-		case "v":
-			*coordY--
-		case "^":
-			*coordY++
-		case "<":
-			*coordX--
-		case ">":
-			*coordX++
-		}
-	}
 
 	// okay how do we "store" these? maybe hash map
-	type coord struct {
-		x, y int32
-	}
-	// problem 1
-	var currentX, currentY int32
-	allResults := make(map[coord]int32) // allResults[coord] = no. of times it occured
-	allResults[coord{currentX, currentY}]++
-	// problem 2
-	var santaX, santaY, roboX, roboY int32
-	dualResults := make(map[coord]int32)
-	dualResults[coord{santaX, santaY}]++
-	isSanta := true // is it santa's turn? will invert for every iteration
+	// part 1
+	var santa image.Point               // santa's location (part one)
+	houses := make(map[image.Point]int) // houses[coord] = no. of times we've passed by a point
+	houses[santa]++
+	// part 2
+	var nuSanta, roboSanta image.Point      // nuSanta and roboSanta's locations
+	dualHouses := make(map[image.Point]int) // dualHouses is houses but for part 2
+	dualHouses[nuSanta]++
+	isSanta := true // is it nuSanta's turn? will invert for every iteration
 
 	for scanner.Scan() {
 		readDirection := scanner.Text()
-		iterateCoordinate(&currentX, &currentY, readDirection)
-		allResults[coord{currentX, currentY}]++
+		iterateCoordinate(&santa, readDirection)
+		houses[santa]++
 		if isSanta {
-			iterateCoordinate(&santaX, &santaY, readDirection)
-			dualResults[coord{santaX, santaY}]++
+			iterateCoordinate(&nuSanta, readDirection)
+			dualHouses[nuSanta]++
 		} else {
-			iterateCoordinate(&roboX, &roboY, readDirection)
-			dualResults[coord{roboX, roboY}]++
+			iterateCoordinate(&roboSanta, readDirection)
+			dualHouses[roboSanta]++
 		}
 		isSanta = !isSanta
-		if err := scanner.Err(); err != nil {
-			return "", "", fmt.Errorf("scanner error: %v", err)
-		}
 	}
-	answer1 = strconv.Itoa(len(allResults))
-	answer2 = strconv.Itoa(len(dualResults))
+	if err = scanner.Err(); err != nil {
+		err = fmt.Errorf("scanner error: %v", err)
+		return
+	}
+	answer1 = strconv.Itoa(len(houses))
+	answer2 = strconv.Itoa(len(dualHouses))
 
 	return answer1, answer2, nil
 }
