e3407bc20875c856689bfe77ab90e340960ec4fa
janreggie <janreggie.delacruz@gmail.com>
2020-01-05 19:15:12 +0800

    Y2019D10. Finally!

--- aoc2019/day10_test.go
+++ aoc2019/day10_test.go
@@ -10,20 +10,47 @@ import (
 func TestDay10(t *testing.T) {
 	assert := assert.New(t)
 	testCases := []structs.TestCase{
-		{Input: "......#.#.\n" +
-			"#..#.#....\n" +
-			"..#######.\n" +
-			".#.#.###..\n" +
-			".#..#.....\n" +
-			"..#....#.#\n" +
-			"#..#....#.\n" +
-			".##.#..###\n" +
-			"##...#..#.\n" +
-			".#....####\n",
-			Result1: "33",
-			Result2: ""},
+		{Input: ".#..##.###...#######\n" +
+			"##.############..##.\n" +
+			".#.######.########.#\n" +
+			".###.#######.####.#.\n" +
+			"#####.##.#.##.###.##\n" +
+			"..#####..#.#########\n" +
+			"####################\n" +
+			"#.####....###.#.#.##\n" +
+			"##.#################\n" +
+			"#####.##.###..####..\n" +
+			"..######..##.#######\n" +
+			"####.##.####...##..#\n" +
+			".#####..#.######.###\n" +
+			"##...#.##########...\n" +
+			"#.##########.#######\n" +
+			".####.#.###.###.#.##\n" +
+			"....##.##.###..#####\n" +
+			".#.#.###########.###\n" +
+			"#.#.#.#####.####.###\n" +
+			"###.##.####.##.#..##\n",
+			Result1: "210",
+			Result2: "802"},
 	}
 	for _, eachCase := range testCases {
 		eachCase.Test(Day10, assert)
+	}
+}
+
+func Benchmark_generateSisonPoints(b *testing.B) {
+	for ii := 0; ii < b.N; ii++ {
+		channel := generateSisonPoints(10, 15)
+		for range channel {
+			// do nothing
+		}
+	}
+}
+func Benchmark_generateSisonPointsSlow(b *testing.B) {
+	for ii := 0; ii < b.N; ii++ {
+		channel := generateSisonPointsSlow(10, 15)
+		for range channel {
+			// do nothing
+		}
 	}
 }
