bd94a73161d2ee31a9b2d14de2bd68d3b831c56b
janreggie <janreggie.delacruz@gmail.com>
2020-01-10 18:04:06 +0800

    Y2015D09: Added more tests + greed + concurrency
    shortestPathGreedy is a greedy algorithm that takes in all
    points on a graph and determines which point yields the
    shortest path using greed: pick the shortest path by choosing
    the towns to visit which are shortest to the current one.
    
    shortestPathPermutative is the algorithm that was
    originally implemented. It's slow. Please don't.
    
    Day09 still uses *Permutative but uses concurrency.
    This should make things fast.
    
    const disconnected signals that a path is not connected.

--- aoc2015/day09.go
+++ aoc2015/day09.go
@@ -6,15 +6,30 @@ import (
 	"math"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/pkg/errors"
 )
+
+// stringSliceBut returns the same slice of strings but without a specified value.
+func stringSliceBut(from []string, except string) []string {
+	out := make([]string, 0, len(from)-1)
+	for _, vv := range from {
+		if vv != except {
+			out = append(out, vv)
+		}
+	}
+	return out
+}
 
 // graph represents a graph
 type graph struct {
 	links map[string]uint // pls no negative distance
 	towns []string
 }
+
+// disconnected is a distance that is unfathomably large
+const disconnected = uint(math.MaxUint32)
 
 func newGraph(scanner *bufio.Scanner) (graph, error) {
 	out := graph{links: make(map[string]uint), towns: make([]string, 0)}
@@ -71,7 +86,7 @@ func (gr *graph) set(a, b string, dist uint) {
 
 // get returns the distance between two towns.
 // If a == b then return 0.
-// If link doesn't exist then return MaxUint.
+// If link doesn't exist then return disconnected.
 func (gr graph) get(a, b string) uint {
 	if a == b {
 		return 0
@@ -83,58 +98,225 @@ func (gr graph) get(a, b string) uint {
 	if dist, ok := gr.links[link]; ok {
 		return dist
 	}
-	return uint(math.MaxUint32)
+	return disconnected
 }
 
-// distance traces the distance returned after traversing all towns in []string.
-// Does not check if towns are in graph.
-func (gr graph) distance(towns []string) uint {
-	if len(towns) <= 1 {
+// in returns true if town is in the graph's list of towns.
+func (gr *graph) in(town string) bool {
+	for _, tt := range gr.towns {
+		if tt == town {
+			return true
+		}
+	}
+	return false
+}
+
+// isValid returns whether the path is a isValid path in graph.
+// If path is empty it will return true.
+func (gr *graph) isValid(path []string) bool {
+	if len(path) == 0 {
+		return true
+	}
+	if len(path) == 1 {
+		return gr.in(path[0])
+	}
+
+	firstTwo := gr.get(path[0], path[1])
+	if len(path) == 2 {
+		return firstTwo != disconnected
+	}
+
+	return gr.isValid(path[1:])
+}
+
+// distance determines the distance returned after traversing all towns in path.
+// Will also return a boolean signifying if path is a valid path.
+// If path is not valid it will return disconnected and false.
+// If path is empty it will return 0 and true.
+func (gr *graph) distance(path []string) (uint, bool) {
+	if len(path) == 0 {
+		return 0, true
+	}
+	if len(path) == 1 {
+		return 0, gr.in(path[0])
+	}
+
+	firstTwo := gr.get(path[0], path[1])
+	if len(path) == 2 {
+		return firstTwo, firstTwo != disconnected
+	}
+
+	remaining, areValid := gr.distance(path[1:])
+	if !areValid {
+		return disconnected, false
+	}
+	return firstTwo + remaining, true
+}
+
+// distanceSimple traces the distance returned after traversing all towns in path.
+// Does not check if the path is even valid.
+// If it isn't then distanceSimple may return a number that is at least disconnectedLink.
+func (gr *graph) distanceSimple(path []string) uint {
+	if len(path) <= 1 {
 		return 0 // there is no distance to be travelled
 	}
-	if len(towns) == 2 {
-		return gr.get(towns[0], towns[1])
+	if len(path) == 2 {
+		return gr.get(path[0], path[1])
 	}
-	return gr.get(towns[0], towns[1]) + gr.distance(towns[1:])
+	return gr.get(path[0], path[1]) + gr.distanceSimple(path[1:])
 }
 
-// generatePermutations generates a channel containing permutations of a string slice
-// and closes once all permutations have been exhausted
-func generatePermutations(data []string) <-chan []string {
+// permutations creates a channel that contains all the possible paths in the graph
+// and will close said channel.
+func (gr *graph) permutations() <-chan []string {
+	permutate := func(c chan []string, inputs []string) {
+		output := make([]string, len(inputs))
+		copy(output, inputs)
+		if gr.isValid(output) {
+			c <- output
+		}
+
+		size := len(inputs)
+		p := make([]int, size+1)
+		for i := 0; i < size+1; i++ {
+			p[i] = i
+		}
+		for i := 1; i < size; {
+			p[i]--
+			j := 0
+			if i%2 == 1 {
+				j = p[i]
+			}
+			tmp := inputs[j]
+			inputs[j] = inputs[i]
+			inputs[i] = tmp
+			output := make([]string, len(inputs))
+			copy(output, inputs)
+			if gr.isValid(output) {
+				c <- output
+			}
+			for i = 1; p[i] == 0; i++ {
+				p[i] = i
+			}
+		}
+	}
+
 	c := make(chan []string)
 	go func(c chan []string) {
 		defer close(c)
-		permutate(c, data)
+		permutate(c, gr.towns)
 	}(c)
 	return c
 }
 
-func permutate(c chan []string, inputs []string) {
-	output := make([]string, len(inputs))
-	copy(output, inputs)
-	c <- output
+// shortestPathPermutative returns the shortest path distance
+// by checking through all permutations of the towns in the graph.
+// This is an expensive operation.
+// If gr has no valid paths it will return disconnected.
+func (gr *graph) shortestPathPermutative() uint {
+	allPermuts := gr.permutations()
 
-	size := len(inputs)
-	p := make([]int, size+1)
-	for i := 0; i < size+1; i++ {
-		p[i] = i
-	}
-	for i := 1; i < size; {
-		p[i]--
-		j := 0
-		if i%2 == 1 {
-			j = p[i]
-		}
-		tmp := inputs[j]
-		inputs[j] = inputs[i]
-		inputs[i] = tmp
-		output := make([]string, len(inputs))
-		copy(output, inputs)
-		c <- output
-		for i = 1; p[i] == 0; i++ {
-			p[i] = i
+	record := disconnected
+	for path := range allPermuts {
+		// guaranteed to be valid.
+		if pathDist := gr.distanceSimple(path); pathDist < record {
+			record = pathDist
 		}
 	}
+
+	return record
+}
+
+// longestPathPermutative returns the longest path distance
+// by checking through all permutations of the towns in the graph.
+// This is an expensive operation.
+// If gr has no valid paths it will return disconnected.
+func (gr *graph) longestPathPermutative() uint {
+	allPermuts := gr.permutations()
+
+	record := uint(0)
+	for path := range allPermuts {
+		// guaranteed to be valid
+		if pathDist := gr.distanceSimple(path); pathDist > record {
+			record = pathDist
+		}
+	}
+
+	if record == 0 {
+		return disconnected
+	}
+	return record
+}
+
+// shortestPathGreedyFrom returns the shortest path distance from some town.
+// If town does not exist return MaxUint32.
+// If the graph is not complete it may perform undefined behavior.
+// This function has issues (see below).
+//
+// Rationale:
+//
+// How about instead of permutating all values we do something a bit better...
+// Suppose there are seven towns A to G that Santa has to go through.
+// Suppose Santa would start at town A.
+// From here, determine which town from the six left is the closest.
+// Suppose town B is the closest. Then the distance travelled so far would be A+B.
+// Continue with the other towns until all towns would have been used.
+//
+// The shortest travelled path from town A to all the other towns
+// will be what shortestPathGreedyFrom will return
+//
+// Such a greedy algorithm may run into issues.
+// Consider a graph that is defined by the following:
+//
+//	A to B = 1
+//	A to C = 4
+//	A to D = 4
+//	B to C = 1
+//	B to D = 10
+//	C to D = 100
+//
+// Suppose we start at A.
+// The shortest path from A would be A -> B -> C -> D, whose total distance is 102.
+// This is not the best choice as it is possible to use A -> C -> B -> D,
+// whose total distance is 15.
+// If the path starts from C however, the path taken would have been C -> B -> A -> D,
+// which would have a total distance of 6.
+func (gr *graph) shortestPathGreedyFrom(town string) uint {
+	// check first if town is in towns
+	if !gr.in(town) {
+		return disconnected
+	}
+
+	remaining := stringSliceBut(gr.towns, town)
+	current := town
+	var totalDistance uint
+
+	for len(remaining) > 0 {
+		// check distances from current to everything in remaining
+		recordDistance, recordTown := disconnected, ""
+		for _, eachDestination := range remaining {
+			if eachDistance := gr.get(current, eachDestination); eachDistance < recordDistance {
+				recordDistance, recordTown = eachDistance, eachDestination
+			}
+		}
+		// now recordTown sounds like the winner.
+		remaining = stringSliceBut(remaining, recordTown)
+		current = recordTown
+		totalDistance += recordDistance
+	}
+
+	return totalDistance
+}
+
+// shortestPathGreedy checks all towns in a graph's towns list to see which is the shortest to go to.
+func (gr *graph) shortestPathGreedy() uint {
+	record := disconnected
+	for _, town := range gr.towns {
+		if pathDistance := gr.shortestPathGreedyFrom(town); pathDistance < record {
+			record = pathDistance
+		}
+	}
+	return record
 }
 
 // Day09 solves the ninth day puzzle
@@ -145,19 +327,16 @@ func Day09(scanner *bufio.Scanner) (answer1, answer2 string, err error) {
 		return
 	}
 
-	allPermuts := generatePermutations(santasGraph.towns)
-
-	highest := uint(0)
-	lowest := uint(math.MaxUint32)
-	for path := range allPermuts {
-		if pathDist := santasGraph.distance(path); pathDist > highest {
-			highest = pathDist
-		} else if pathDist < lowest {
-			lowest = pathDist
-		}
-	}
-
-	answer1 = strconv.FormatUint(uint64(lowest), 10)
-	answer2 = strconv.FormatUint(uint64(highest), 10)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		answer1 = strconv.FormatUint(uint64(santasGraph.shortestPathPermutative()), 10)
+		wg.Done()
+	}()
+	go func() {
+		answer2 = strconv.FormatUint(uint64(santasGraph.longestPathPermutative()), 10)
+		wg.Done()
+	}()
+	wg.Wait()
 	return
 }
--- aoc2015/day09_test.go
+++ aoc2015/day09_test.go
@@ -9,6 +9,117 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var day09sampleGraph = func() graph {
+	const input = "London to Dublin = 464\n" +
+		"London to Belfast = 518\n" +
+		"Dublin to Belfast = 141\n"
+
+	// guaranteed not to error
+	gr, _ := newGraph(bufio.NewScanner(strings.NewReader(input)))
+	return gr
+}()
+
+var day09myGraph = func() graph {
+	const input = "Faerun to Norrath = 129\n" +
+		"Faerun to Tristram = 58\n" +
+		"Faerun to AlphaCentauri = 13\n" +
+		"Faerun to Arbre = 24\n" +
+		"Faerun to Snowdin = 60\n" +
+		"Faerun to Tambi = 71\n" +
+		"Faerun to Straylight = 67\n" +
+		"Norrath to Tristram = 142\n" +
+		"Norrath to AlphaCentauri = 15\n" +
+		"Norrath to Arbre = 135\n" +
+		"Norrath to Snowdin = 75\n" +
+		"Norrath to Tambi = 82\n" +
+		"Norrath to Straylight = 54\n" +
+		"Tristram to AlphaCentauri = 118\n" +
+		"Tristram to Arbre = 122\n" +
+		"Tristram to Snowdin = 103\n" +
+		"Tristram to Tambi = 49\n" +
+		"Tristram to Straylight = 97\n" +
+		"AlphaCentauri to Arbre = 116\n" +
+		"AlphaCentauri to Snowdin = 12\n" +
+		"AlphaCentauri to Tambi = 18\n" +
+		"AlphaCentauri to Straylight = 91\n" +
+		"Arbre to Snowdin = 129\n" +
+		"Arbre to Tambi = 53\n" +
+		"Arbre to Straylight = 40\n" +
+		"Snowdin to Tambi = 15\n" +
+		"Snowdin to Straylight = 99\n" +
+		"Tambi to Straylight = 70\n"
+
+	// guaranteed not to error
+	gr, _ := newGraph(bufio.NewScanner(strings.NewReader(input)))
+	return gr
+}()
+
+func Test_graph_get(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		gr   *graph
+		a, b string
+		want uint
+	}{
+		{&day09sampleGraph, "London", "Belfast", 518},
+		{&day09sampleGraph, "Belfast", "London", 518},
+		{&day09sampleGraph, "London", "London", 0},
+		{&day09sampleGraph, "London", "Somewhere", disconnected},
+	}
+	for _, tt := range tests {
+		assert.Equal(tt.want, tt.gr.get(tt.a, tt.b), tt.a, tt.b, tt.want)
+	}
+}
+
+func Test_graph_distance(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		gr    *graph
+		towns []string
+		want  uint
+	}{
+		{&day09sampleGraph, []string{"Dublin", "London", "Belfast"}, 982},
+		{&day09sampleGraph, []string{"London", "Dublin", "Belfast"}, 605},
+		{&day09sampleGraph, []string{"London", "Belfast", "Dublin"}, 659},
+		{&day09sampleGraph, []string{"Dublin", "Belfast", "London"}, 659},
+		{&day09sampleGraph, []string{"Belfast", "Dublin", "London"}, 605},
+		{&day09sampleGraph, []string{"Belfast", "London", "Dublin"}, 982},
+	}
+	for _, tt := range tests {
+		assert.Equal(tt.want, tt.gr.distanceSimple(tt.towns), tt.towns)
+	}
+}
+
+func Test_graph_greedyShortestPathFrom(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		gr   *graph
+		town string
+		want uint
+	}{
+		{&day09sampleGraph, "Dublin", 659},
+		{&day09sampleGraph, "London", 605},
+		{&day09sampleGraph, "Belfast", 605},
+	}
+	for _, tt := range tests {
+		assert.Equal(tt.want, tt.gr.shortestPathGreedyFrom(tt.town), tt.town)
+	}
+}
+
+func Test_graph_shortestPathGreedy(t *testing.T) {
+	assert := assert.New(t)
+	tests := []struct {
+		gr   *graph
+		want uint
+	}{
+		{&day09sampleGraph, 605},
+		{&day09myGraph, 207}, // the shortest one we have.
+	}
+	for _, tt := range tests {
+		assert.Equal(tt.want, tt.gr.shortestPathGreedy(), tt.gr)
+	}
+}
+
 func TestDay09(t *testing.T) {
 	assert := assert.New(t)
 	testCases := []structs.TestCase{
@@ -86,5 +197,17 @@ func BenchmarkDay09(b *testing.B) {
 		"Tambi to Straylight = 70\n"
 	for ii := 0; ii < b.N; ii++ {
 		Day09(bufio.NewScanner(strings.NewReader(input)))
+	}
+}
+
+func Benchmark_graph_shortestPathPermutative(b *testing.B) {
+	for ii := 0; ii < b.N; ii++ {
+		day09myGraph.shortestPathPermutative()
+	}
+}
+
+func Benchmark_graph_shortestPathGreedy(b *testing.B) {
+	for ii := 0; ii < b.N; ii++ {
+		day09myGraph.shortestPathGreedy()
 	}
 }
