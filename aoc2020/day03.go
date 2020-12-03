dbea13800161dc313cd485ff321ac0e6f24bfcd6
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2020-12-03 14:04:08 +0800

    Y2020D03

---
+++ aoc2020/day03.go
@@ -0,0 +1,114 @@
+package aoc2020
+
+import (
+	"bufio"
+	"strconv"
+
+	"github.com/pkg/errors"
+)
+
+// tobogganForest represents a Toboggan forest
+type tobogganForest struct {
+	width  int      // width of the forest, after which, repeats itself
+	height int      // number of rows the forest has
+	forest [][]bool // true if a tree exists here
+}
+
+// readTobogganForest reads to forest data from a bufio.Scanner.
+// If it is uneven, reallocate...
+func readTobogganForest(scanner *bufio.Scanner) (*tobogganForest, error) {
+	result := &tobogganForest{}
+	result.forest = make([][]bool, 0)
+	for scanner.Scan() {
+		readRow := []rune(scanner.Text())
+		currentRow := make([]bool, 0)
+		for _, unit := range readRow {
+			switch unit {
+			case '#':
+				currentRow = append(currentRow, true)
+			case '.':
+				currentRow = append(currentRow, false)
+			default:
+				return nil, errors.Errorf("couldn't read char %c at row `%s`", unit, scanner.Text())
+			}
+		}
+
+		result.height++
+		if result.width != 0 && result.width != len(currentRow) {
+			return nil, errors.Errorf("width should be %v but row %s is length %v", result.width, scanner.Text(), len(currentRow))
+		}
+		result.width = len(currentRow)
+		result.forest = append(result.forest, currentRow)
+	}
+
+	return result, nil
+}
+
+// read reads at coordinates x, y where x is distance from first column and y distance from first row
+// and returns whether a tree is found there.
+// x, y start at zero.
+// If y is out of bounds or x is negative, return False.
+func (forest *tobogganForest) read(x, y int) bool {
+	if x < 0 {
+		return false
+	}
+	if y < 0 || y >= forest.height {
+		return false
+	}
+
+	// obvious check if width or height are empty
+	if forest.width == 0 || forest.height == 0 {
+		return false
+	}
+
+	return forest.forest[y][x%forest.width]
+}
+
+// traverse traverses through the forest starting at (x,y) by taking r steps to the right and d steps downwards
+// for every turn, and ultimately counting how many trees have been encountered.
+func (forest *tobogganForest) traverse(r, d, x, y int) int {
+	if r == 0 && d == 0 { // because this will make traverse run indefinitely
+		if forest.read(x, y) {
+			return 1
+		}
+		return 0
+	}
+
+	if y > forest.height || y < 0 || x < 0 {
+		return 0
+	}
+	if forest.read(x, y) {
+		return 1 + forest.traverse(r, d, x+r, y+d)
+	}
+	return forest.traverse(r, d, x+r, y+d)
+}
+
+// Day03 solves the third day puzzle "Toboggan Trajectory"
+//
+// Input
+//
+// A file which contains a description of a forest expressed in dots and hashes. For example:
+//
+//  some sample input indented to become a code block
+//
+// The file should only contain `.`, `#`, and newlines. All lines are of equal length.
+func Day03(scanner *bufio.Scanner) (answer1, answer2 string, err error) {
+	forest, err := readTobogganForest(scanner)
+	if err != nil {
+		err = errors.Wrap(err, "couldn't read from scanner")
+		return
+	}
+
+	// While this can be solved concurrently,
+	// the overhead of using channels far exceeds the runtime of the five traverse calls below,
+	// hence doing so might not be the wisest thing to do.
+	r1d1 := forest.traverse(1, 1, 0, 0)
+	r3d1 := forest.traverse(3, 1, 0, 0)
+	r5d1 := forest.traverse(5, 1, 0, 0)
+	r7d1 := forest.traverse(7, 1, 0, 0)
+	r1d2 := forest.traverse(1, 2, 0, 0)
+
+	answer1 = strconv.Itoa(r3d1)
+	answer2 = strconv.Itoa(r1d1 * r3d1 * r5d1 * r7d1 * r1d2)
+	return
+}
